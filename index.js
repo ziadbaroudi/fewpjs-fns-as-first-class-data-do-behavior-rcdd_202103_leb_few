1eb19923bd5e05221c4dffe112b70c71bfeeb0b4
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-14 00:22:05 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(t){
+  let time_number = parseInt(t, 10);
+  if (time_number < 12){
+    return "Good Morning"
+  }
+  
+  if (time_number<17) {
+    return "Good Afternoon"
+  }
+  
+    return "Good Evening"
+  
+}
+
 /* Write your implementation of displayMessage() */
+
+
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
